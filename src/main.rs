b42b116b3f9df20293d76131b0c41952112cbff8
nnlgsakib <nlgarts@gmail.com>
2025-07-28 15:08:15 +0600

    learnt enum

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ mod print;
 mod scaler_types;
 mod struct_l;
 mod var;
+mod enum_l;
 
 fn main() {
     print::print_func();
     struct_l::student_printer();
     var::var_practice();
     scaler_types::scaler_types();
-    compound_types::cmp_type()
+    compound_types::cmp_type();
+    enum_l::col_def()
 }
